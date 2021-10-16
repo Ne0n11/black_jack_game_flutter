da1c0f8ede8990f736da1dcfd73ff2947f93d54c
Arthur Puha <pugaartur@gmail.com>
2021-10-16 10:38:13 +0300

    init

---
+++ lib/screens/black_jack_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class BlackJackScreen extends StatefulWidget {
+
+  @override
+  _BlackJackScreenState createState() => _BlackJackScreenState();
+}
+
+class _BlackJackScreenState extends State<BlackJackScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
